1710b851d71dcdb25e31d1cc23ac29918b6055ff
Ade Mugni Rusmana <m.rhuzmana@gmail.com>
2023-12-04 23:58:35 +0700

    :sparkles: Create profile menu

--- src/routes/templates/authed.js
+++ src/routes/templates/authed.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 
 import { setSideMenu } from './../../redux/sideMenuSlice';
@@ -6,12 +6,18 @@ import { logOut } from './../../redux/authSlice';
 
 import Dash from './../../assets/svgs/icon-dash-admin.svg'
 import { useNavigate } from "react-router";
+import { useOutsideClick } from "../../config/hook";
 
 const AuthedTemplate = ({ children }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const sideMenu = useSelector(({ sideMenu }) => sideMenu);
   const auth = useSelector(({ auth }) => auth);
+  const [showProfileMenu, setShowProfileMenu] = useState(false);
+  const handleProfileRef = () => {
+    setShowProfileMenu(false);
+  }
+  const profileRef = useOutsideClick(handleProfileRef);
   const menus = [
     {
       title: 'Dashboard',
@@ -118,7 +124,7 @@ const AuthedTemplate = ({ children }) => {
 
   return (
     <div className="w-screen h-screen relative flex flex-col hide-scroll p-0 m-0"> 
-      <div className={`${setSideMenuByStatus().width_top} h-[60px] bg-sky-400 flex flex-row`}>
+      <div className={`${setSideMenuByStatus().width_top} h-[60px] bg-sky-400 flex flex-row ${showProfileMenu ? 'z-[60]' : 'z-[0]'}`}>
         <div className={`w-[250px] h-[56px] fixed desktop:absolute ${setSideMenuByStatus().header_left} top-0 transition-all duration-300 ease-in-out flex flex-row items-center justify-center text-white font-bold text-xl`}>SaveMe</div>
         <div className={`h-[56px] fixed desktop:absolute ${setSideMenuByStatus().header_right} top-0 transition-all duration-300 ease-in-out flex flex-row`}>
           <div
@@ -129,11 +135,34 @@ const AuthedTemplate = ({ children }) => {
             <img src={Dash} className="w-full" alt="dash"/>
             <img src={Dash} className="w-full" alt="dash"/>
           </div>
-          <div className={`w-full ${setSideMenuByStatus().header_right_content} tablet:flex items-center justify-end pr-5 z-[1]`}>
-            <span
-              className="cursor-pointer text-white font-bold"
-              onClick={onLogout}
-            >Logout</span>
+          <div className={`w-full ${setSideMenuByStatus().header_right_content} tablet:flex items-center justify-end pr-5 z-[1] relative`}>
+            <div
+              ref={profileRef}
+              className="w-fit cursor-pointer text-white font-bold flex flex-col relative"
+              onClick={() => setShowProfileMenu(!showProfileMenu)}
+            >
+              {auth?.data?.profile?.name}
+              <div className={`w-[250px] min-h-[50px] flex flex-col px-3 py-2 gap-2 font-bold text-xs text-sky-500 bg-white rounded shadow-lg absolute top-11 right-0 transition-all duration-300 ease-in-out border border-gray-300 ${showProfileMenu ? 'opacity-100 z-[60]' : 'opacity-0 z-[1]'}`}>
+                <span
+                  className="transition-all duration-500 ease-in-out hover:text-white hover:bg-sky-500 p-2 rounded cursor-pointer"
+                  onClick={() => navigate('/profile')}
+                ><i className="fa-solid fa-user w-[25px]"></i> Profile</span>
+                <span
+                  className="transition-all duration-500 ease-in-out hover:text-white hover:bg-sky-500 p-2 rounded cursor-pointer"
+                  onClick={() => navigate('/change-password')}
+                ><i className="fa-solid fa-lock w-[25px]"></i> Change Password</span>
+                <span
+                  className="transition-all duration-500 ease-in-out hover:text-white hover:bg-sky-500 p-2 rounded cursor-pointer"
+                  onClick={() => navigate('/update-username')}
+                ><i className="fa-solid fa-id-card w-[25px]"></i> Update Username</span>
+                <span
+                  className="transition-all duration-500 ease-in-out hover:text-white hover:bg-sky-500 p-2 rounded cursor-pointer"
+                  onClick={() => {
+                    onLogout()
+                  }}
+                ><i className="fa-solid fa-right-from-bracket w-[25px]"></i> Logout</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
